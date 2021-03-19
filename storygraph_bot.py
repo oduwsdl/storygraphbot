258cdf6187305c6db44fb3c660c02c75bfca8a33
kritika_garg <kgarg.kritika@gmail.com>
2021-03-18 23:03:12 -0400

    fixed updates

--- storygraph_bot.py
+++ storygraph_bot.py
@@ -13,7 +13,7 @@ def get_data():
 	a = os.system(cmd)
 	read_file = open("graphs_tmp/current_storygraphdata.json", "r")
 	data = json.load(read_file)
-	return data
+	return(data)
 
 
 def get_cache(date):
@@ -34,8 +34,7 @@ def new_cache(date):
 	cache = {
 			current_date: {  
 				"start_datetime": current_date+' 00:00:00',
-				#"end_datetime": "",
-				#"no_of_updates": init 0 then inc++,
+				"end_datetime": "",
 				"stories": [
 					]
 				}
@@ -49,9 +48,9 @@ def overlap_stories(first, second):
 	intersection = float(len(first & second))
 	minimum = min(len(first), len(second))
 	if( minimum != 0 ):
-		return round(intersection/minimum, 4)
+		return(round(intersection/minimum, 4))
 	else:
-		return 0 
+		return(0) 
 
 
 def get_stories_uri_datetimes(jdata): 
@@ -65,7 +64,7 @@ def get_stories_uri_datetimes(jdata):
 
 
 def map_cache_stories(cache, data):
-	map_cachestories = {}	
+	map_cachestories = {} 	
 	topstory_incache = True
 
 	stories_uri_dts =  get_stories_uri_datetimes(data["story_clusters"])
@@ -82,8 +81,12 @@ def map_cache_stories(cache, data):
 
 			if maxcoeff >= args.overlap_threshold:
 				c_indx = coeff_list.index(maxcoeff)
-				supdate = s_uridt - cachedstories_uri_dts[c_indx]	
-				map_cachestories.update({c_indx: [sn, supdate]})
+				story_id = cache_stories[c_indx]["story_id"]
+				new_graphs = s_uridt - cachedstories_uri_dts[c_indx]
+				update = {"sgtkdata_story": sn, "new_graphs": list(new_graphs)}
+				#map_cachestories.update({c_indx: [sn, supdate]})
+
+				map_cachestories.update({story_id: update})
 
 			elif sn == 0:
 				topstory_incache = False
@@ -91,8 +94,27 @@ def map_cache_stories(cache, data):
 	
 			if len(map_cachestories) == len(cachedstories_uri_dts):
 				break
-
+	#mapperfile
+	mapper_update(map_cachestories)
 	return(map_cachestories, topstory_incache)
+
+
+
+def mapper_update(map_cachestories):
+	''' mapping json shows the update on the tracked stories at each timestamp'''
+	mapper_file = "mapper_"+ date
+	if os.path.isfile(mapper_file):
+		try:
+			mapper = json.load(open(mapper_file, "r"))
+		except Exception as e:
+			print(f"Error: Please clear the {mapper_file} created in earlier test run. cmd: rm -f {mapper_file}")
+			sys.exit()
+	else:
+		mapper = {}
+
+	mapper[data["end_date"]] = map_cachestories		
+	json.dump(mapper, open(mapper_file, 'w'))
+
 
 
 
@@ -108,51 +130,59 @@ def get_topstory(stories):
 
 
 def new_story(top_story, cache_stories):
-	story_no = f'{date.replace("-","")}_{len(cache_stories)}'
-	top_story["Story ID"] = story_no
-	story_graph = top_story["graph_ids"][0]	
+	#print("Creating 1 new thread for new top story")			
+	story_id = f'{date.replace("-","")}_{len(cache_stories)}'
+	top_story["story_id"] = story_id
+	top_story["reported_graphs"] = []
+	story_graph = top_story["graph_ids"][0]
+	#post story to file
+	post_story(story_id, story_graph)	
+	#update cache
+	top_story["reported_graphs"].append(story_graph)
+	cache_stories.append(top_story) 	
+	return(story_id, story_graph)
 
-	#print("Creating 1 new thread for new top story (Story"+ str(story_no)+")")			
-	formatted_story = print_story(story_no, story_graph) #print top graph of top_story
-
-	top_story["reported_graph"] = formatted_story
-	cache_stories.append(top_story) 
-	return(formatted_story)
 
 
+def get_story_cache(story_id):
+	for indx in range(len(cache_stories)):
+		story = cache_stories[indx]
+		if story["story_id"] == story_id:
+			return(story, indx)
 
-def update_story(cache_sidx, data_story):
-	updated_ids = []
-	story_cache = cache_stories[cache_sidx]
-	story_no = story_cache["Story ID"]
-	data_sidx, update = data_story
 
-	if update:
-		story_data = stories[data_sidx] 				#get story's current update from data
+
+def update_story(story_id, update):
+	story_cache, c_indx = get_story_cache(story_id)
+	data_sidx = update["sgtkdata_story"]
+	new_graphs = update['new_graphs']
+
+	if new_graphs:
+		story_data = stories[data_sidx] 			
 		story_graphs = story_data["graph_ids"]
 
-		if story_graphs[0] != story_cache["graph_ids"][0]:
+		if story_graphs[0]['id'] != story_cache["graph_ids"][0]['id']:
 			latest_graph = story_graphs[0]
 		else: 
 			#top snapshot hasnt changed
-			latest_graph_uri = sorted(update, reverse=True)[0]
+			latest_graph_uri = sorted(new_graphs, reverse=True)[0]
 			latest_story_graphs = [dic for dic in story_graphs if dic["graph_uri_local_datetime"]==latest_graph_uri] 
 			l_id = sorted(latest_story_graphs, key=lambda k: int(k['id'].split("-")[1]), reverse=True) #id used identify graphs
 			latest_graph = l_id[0]
-			
-		formatted_story = print_story(story_no, latest_graph)
+		
+		post_story(story_id, latest_graph)	
 		#update_cache
-		story_data["Story ID"] = story_no	
-		story_data["reported_graph"] = formatted_story 											
-		cache_stories[cache_sidx] = story_data 				
-		updated_ids.append(story_no)
-
-	return(updated_ids)	
-
+		story_data["story_id"] = story_id	
+		story_data["reported_graphs"] = story_cache["reported_graphs"]												
+		story_data["reported_graphs"].append(latest_graph)													
+		cache_stories[c_indx] = story_data 				
+		return(True)	
 
 
-def print_story(story_no, story_graph):
-	story_fname = "output/story_"+str(story_no)+".txt"
+
+def post_story(story_id, story_graph):
+	'''Post story to file'''
+	story_fname = "output/story_"+str(story_id)+".txt"
 	if os.path.exists(story_fname):
 		mode = 'a+' 
 	else:
@@ -160,19 +190,24 @@ def print_story(story_no, story_graph):
 	story_file = open(story_fname, mode)
 
 	#format graph
-	formatted_graph = {
-		"Story ID" : story_no,
-		"Title" : story_graph['max_node_title'],		
-		"Avg Degree" : story_graph['avg_degree'],
-		"Graph URI": story_graph['graph_uri']
-			}	
+	formatted_graph = pretty_print_graph(story_id, story_graph)	
 
 	#print story to file		
 	for k,v in formatted_graph.items(): 
 		story_file.write("{}:{}\n".format(k,v))			
 	story_file.write("\n")
-	return(formatted_graph)
 
+
+
+def pretty_print_graph(story_id, story_graph):
+	#format graph
+	formatted_graph = {
+		"Story ID" : story_id,
+		"Title" : story_graph['max_node_title'],		
+		"Avg Degree" : story_graph['avg_degree'],
+		"Graph URI": story_graph['graph_uri']
+			}	
+	return(formatted_graph)
 
 
 
@@ -192,15 +227,16 @@ if __name__ == "__main__":
 	data = get_data()
 	date = list(data["story_clusters"])[0] 
 	cache = get_cache(date)
+	cache_stories = cache[date]['stories']
 
 	map_cachestories, st0_incache = map_cache_stories(cache, data)
+	#print(map_cachestories)
 	stories = data["story_clusters"][date]["stories"]	
 
 	if stories == []:
 		sys.exit("No stories in the new data")
 
 	top_story = get_topstory(stories)
-	cache_stories = cache[date]['stories']
 
 	print("Activation degree: "+str(args.activation_degree))
 	print("Overlap threshold: "+str(args.overlap_threshold))
@@ -208,15 +244,16 @@ if __name__ == "__main__":
 	new_story_id = None
 	if top_story:		
 		if not st0_incache or not map_cachestories: 			#add story to cache
-			new_story = new_story(top_story, cache_stories)
-			new_story_id = new_story["Story ID"]	
+			new_story_id, new_story = new_story(top_story, cache_stories)
 	print(f'New top story id: {new_story_id}')
 
 
 	updated_ids = []
 	if map_cachestories:   	
-		for cache_sidx, data_story in map_cachestories.items():
-			updated_ids = update_story(cache_sidx, data_story)	
+		for story_id, update in map_cachestories.items():
+			is_updated = update_story(story_id, update)
+			if is_updated:
+				updated_ids.append(story_id)	
 
 	if updated_ids == []:
 		updated_ids = None
@@ -226,17 +263,30 @@ if __name__ == "__main__":
  							
 
 	#dump_cache 
+	cache[date]["end_datetime"] = data["end_date"]
 	json.dump(cache, open('cache/cache_'+date, 'w'))	
 
 
 	#print stories on console
 	print("\nAll Stories:\n")
-	for story in cache_stories:
-		for k,v in story["reported_graph"].items(): 
+	for story in cache_stories:	
+		story_id = story["story_id"]
+		reported_graphs = story["reported_graphs"]
+
+		formatted_story = pretty_print_graph(story_id, reported_graphs[-1])
+		for k,v in formatted_story.items(): 
 			print(f'\t{k}: {v}')
-		print('')		
+
+		if len(reported_graphs) > 1:	
+			print(f'\t\tHistory:')
+			for graph in reported_graphs[:-1]:
+				formatted_story = pretty_print_graph(story_id, graph)
+				for k,v in formatted_story.items():
+					if k!="Story ID": 
+						print(f'\t\t\t{k}: {v}')			
+		print('')
+
 
 	#if len(map_cachestories) < len(cache_stories):
 	#print("\nThere are no updates on the other stories")
-
 
